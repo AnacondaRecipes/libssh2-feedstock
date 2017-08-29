789d5bd923a3b70d7348a0b497d5710706c01f95
Ray Donnelly <mingw.android@gmail.com>
2017-08-29 00:36:31 -0700

    Break build cycle using a binary cmake
    curl->libssh2->cmake->curl

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 set PATH=%PREFIX%\cmake-bin\bin;%PATH%
 
 mkdir build && cd build
@@ -12,3 +13,27 @@ IF %ERRORLEVEL% NEQ 0 exit 1
 
 cmake --build . --config Release --target INSTALL
 IF %ERRORLEVEL% NEQ 0 exit 1
+=======
+mkdir cycle-breaker
+pushd cycle-breaker
+  if %ARCH% == 32 goto skip_64
+  powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://cmake.org/files/v3.8/cmake-3.8.2-win64-x64.zip', 'cmake.zip') }"
+  unzip cmake.zip
+  set PATH=%CD%\cmake-3.8.2-win64-x64\bin;%PATH%
+  goto done
+:skip_64
+  powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://cmake.org/files/v3.8/cmake-3.8.2-win32-x86.zip', 'cmake.zip') }"
+  unzip cmake.zip
+  set PATH=%CD%\cmake-3.8.2-win32-x86\bin;%PATH%
+:done
+popd
+
+mkdir build
+pushd build
+  cmake .. -G "NMake Makefiles"                     ^
+           -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%  ^
+           -DCMAKE_BUILD_TYPE=Release               ^
+           -DBUILD_SHARED_LIBS=OFF
+  cmake --build . --config Release --target INSTALL
+popd
+>>>>>>> Break build cycle using a binary cmake
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+<<<<<<< HEAD
 # We use a repackaged cmake from elsewhere to break a build cycle.
 export PATH=${PREFIX}/cmake-bin/bin:${PATH}
 
@@ -27,3 +28,55 @@ cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
 make -j${CPU_COUNT}
 # ctest  # fails on the docker image
 make install
+=======
+mkdir cycle-breaker
+pushd cycle-breaker
+  if [[ $(uname) == Darwin ]]; then
+    curl -SLO https://cmake.org/files/v3.8/cmake-3.8.2-Darwin-x86_64.tar.gz
+    tar -xf cmake-3.8.2-Darwin-x86_64.tar.gz
+    export PATH=${PWD}/cmake-3.8.2-Darwin-x86_64/CMake.app/Contents/bin:${PATH}
+  elif [[ $(uname) == Linux ]]; then
+    if [[ $(uname -m) == x86_64 ]]; then
+      wget https://cmake.org/files/v3.8/cmake-3.8.2-Linux-x86_64.tar.gz
+      tar -xf cmake-3.8.2-Darwin-x86_64.tar.gz
+      export PATH=${PWD}/cmake-3.8.2-Linux-x86_64/bin:${PATH}
+    elif [[ $(uname -m) == i686 ]]; then
+      # There is no binary release of cmake for linux-32
+      declare -a _cmake_and_deps
+      _base_url=https://repo.continuum.io/pkgs/free/linux-32
+      _cmake_and_deps+=(cmake-3.6.3-0)
+      _cmake_and_deps+=(curl-7.54.1-0)
+      _cmake_and_deps+=(expat-2.1.0-0)
+      _cmake_and_deps+=(krb5-1.13.2-0)
+      _cmake_and_deps+=libssh2-1.8.0-0)
+      _cmake_and_deps+=(ncurses-5.9-10)
+      _cmake_and_deps+=(openssl-1.0.2l-0)
+      _cmake_and_deps+=(xz-5.2.3-0)
+      _cmake_and_deps+=(zlib-1.2.11-0)
+      _cmake_and_deps+=(bzip2-1.0.6-3)
+      for _pkg in "${_cmake_and_deps[@]}"; do
+        wget ${_base_url}/${_pkg}.tar.bz2
+        tar -xf ${_pkg}.tar.bz2
+      done
+      export PATH=${PWD}/bin:${PATH}
+    else
+      echo "Please figure out where to get a pre-build binary of CMake for your platform:"
+      uname -a
+      echo "This is necessary to break a dependency cycle."
+      exit 1
+    fi
+  fi
+popd
+
+mkdir build
+pushd build
+  for _shared in "OFF ON", do
+    cmake -DCMAKE_INSTALL_PREFIX=${PREFIX}  \
+          -DBUILD_SHARED_LIBS=${_shared}    \
+          -DCMAKE_INSTALL_LIBDIR=lib        \
+          ..
+    make -j${CPU_COUNT} ${VERBOSE_CM}
+    make install
+  done
+popd
+>>>>>>> Break build cycle using a binary cmake
