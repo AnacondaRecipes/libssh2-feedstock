fb3b46aeb60ad02b421e3a7a2055eea23ebdcc68
Michael Sarahan <msarahan@gmail.com>
2017-11-21 13:18:43 -0600

    remove vc features; use VS cmake generator; output shared libs

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,10 +1,13 @@
 set PATH=%PREFIX%\cmake-bin\bin;%PATH%
 
+set CFLAGS=
+set CXXFLAGS=
+
 mkdir build
 pushd build
-  cmake .. -G "NMake Makefiles"                     ^
+  cmake .. -G "%CMAKE_GENERATOR%"                     ^
            -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%  ^
            -DCMAKE_BUILD_TYPE=Release               ^
-           -DBUILD_SHARED_LIBS=OFF
+           -DBUILD_SHARED_LIBS=ON
   cmake --build . --config Release --target INSTALL
 popd
