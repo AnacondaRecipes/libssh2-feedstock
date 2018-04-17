8fbfa6f95ee861b144d822d029949efdc5601f45
Michael Sarahan <msarahan@gmail.com>
2018-04-17 16:59:56 -0500

    clean up bad merge

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,19 +1,5 @@
 set PATH=%PREFIX%\cmake-bin\bin;%PATH%
 
-<<<<<<< HEAD
-mkdir build && cd build
-
-cmake -G "%CMAKE_GENERATOR%" ^
-		  -D BUILD_SHARED_LIBS=ON ^
-			-D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-			-D ENABLE_ZLIB_COMPRESSION=ON ^
-			-D CMAKE_BUILD_TYPE=Release ^
-			%SRC_DIR%
-IF %ERRORLEVEL% NEQ 0 exit 1
-
-cmake --build . --config Release --target INSTALL
-IF %ERRORLEVEL% NEQ 0 exit 1
-=======
 set CFLAGS=
 set CXXFLAGS=
 
@@ -22,7 +8,8 @@ pushd build
   cmake .. -G "%CMAKE_GENERATOR%"                     ^
            -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%  ^
            -DCMAKE_BUILD_TYPE=Release               ^
+			     -DENABLE_ZLIB_COMPRESSION=ON ^
            -DBUILD_SHARED_LIBS=ON
   cmake --build . --config Release --target INSTALL
+IF %ERRORLEVEL% NEQ 0 exit 1
 popd
->>>>>>> remove vc features; use VS cmake generator; output shared libs
